ea010451c6fb77307becf5adef19b103f63a9eda
khenson3 <khenson@multidataservices.com>
2025-07-10 14:16:44 -0400

    Updates to visual layout -add images to menu -styling updates

--- src/menuLoad.js
+++ src/menuLoad.js
@@ -1,3 +1,8 @@
+import margeritaImg from './assets/margerita.jpg';
+import neopolitanImg from './assets/neopolitan.jpg';
+import peppperoniImg from './assets/pepperoni.jpg';
+import prosciuttoImg from './assets/prosciutto.jpg';
+
 export function menuLoad() {
   // Create contentDiv if it doesn't exist
   let contentDiv = document.querySelector("#content");
@@ -5,16 +10,28 @@ export function menuLoad() {
     contentDiv = document.createElement("div");
     contentDiv.id = "content";
   }
-
+  
   // Use innerHTML to insert HTML content
   contentDiv.innerHTML = `
     <h1>Menu</h1>
     <h2>Pizzas</h3>
     <div id="pizzas">
-        <h4>Margerita</h4>
-        <h4>Napolitano</h4>
-        <h4>Pepperoni</h4>
-        <h4>Proscuitto</h4>
+        <div id="margherita">
+            <h3>Margherita</h3>
+            <img src="${margeritaImg}" alt="Margerita Pizza" class="pizzaImages">
+        </div>
+        <div id="Neopolitan">
+            <h3>Neopolitan</h3>
+             <img src="${neopolitanImg}" alt="Neoplitan Pizza" class="pizzaImages">
+        </div>
+        <div id="Pepperoni">
+            <h3>Pepperoni</h3>
+            <img src="${peppperoniImg}" alt="Pepperoni Pizza" class="pizzaImages">
+        </div>
+        <div id="Proscuitto">
+            <h3>Proscuitto</h3>
+            <img src="${prosciuttoImg}" alt="Prosciutto Pizza" class="pizzaImages">
+        </div>
     </div>
   `;
 
